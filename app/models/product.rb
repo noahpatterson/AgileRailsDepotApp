65ed070d222a7e74d4c4a79cf9c61fd0677e7a16
noahpatterson <noah@noah-patterson.com>
2014-02-12 07:34:01 -0500

    add validation

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ActiveRecord::Base
+
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  
+end
